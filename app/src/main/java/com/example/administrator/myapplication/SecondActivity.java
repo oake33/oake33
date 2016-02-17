a50ba3d45df63e5a43c723dfec99a5f3cff7d074
tangceshi <13331054011@163.com>
2016-02-17 22:47:45 +0800

    第一次简单测试

---
+++ app/src/main/java/com/example/administrator/myapplication/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SecondActivity extends AppCompatActivity {
+    private ImageView ic_show;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fab.setDrawingCacheEnabled(true);
+                fab.getDrawingCache();
+
+                ic_show.setImageBitmap(fab.getDrawingCache());
+            }
+        });
+        ic_show = (ImageView) findViewById(R.id.ic_show);
+
+
+    }
+
+}
